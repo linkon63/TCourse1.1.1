9587303c28d5fffde8ad14066ffa7f9f4a4fec78
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2023-02-27 17:48:08 +0600

    d

--- C-13 Intro Programming/c-13 hello.cpp
+++ C-13 Intro Programming/c-13 hello.cpp
@@ -29,12 +29,27 @@ int main()
 //    cout<<"accessing string as a char sName[index]:"<<sName[6]<<endl;
 
     /// pattern show
-    cout<<"**           ##"<<endl;
-    cout<<"**           ##"<<endl;
-    cout<<"**           ##"<<endl;
-    cout<<"**           ##"<<endl;
-    cout<<"*********    ##"<<endl;
-    cout<<"*********    ##"<<endl;
+//    cout<<"**           ##"<<endl;
+//    cout<<"**           ##"<<endl;
+//    cout<<"**           ##"<<endl;
+//    cout<<"**           ##"<<endl;
+//    cout<<"*********    ##"<<endl;
+//    cout<<"*********    ##"<<endl;
+
+/// YOUR NAME ADDRESS PHONE SHOW IN CONSOLE
+
+///             MY DETAILS
+/// NAME : ----------
+/// EDU  :
+/// .....
+/// .....
+
+
+/// ADDING SUB MUL DIVING AND MODULUS
+/// A%B = 10%20 = 0
+/// MODULES = A%B = 7/5 = 2
+
+
 
 // int - stores integers (whole numbers), without decimals, such as 123 or -123
 // double - stores floating point numbers, with decimals, such as 19.99 or -19.99
